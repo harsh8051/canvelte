748206c870b0a5b934e8c8cd22eb05a8fba6be5f
harsh8051 <105968770+harsh8051@users.noreply.github.com>
2023-05-24 21:13:23 +0530

    Basic Canvas is added

---
+++ src/components/Canvas.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { onMount } from "svelte";
+
+  let canvas;
+
+  onMount(() => {
+    // Code to initialize and interact with the canvas
+  });
+</script>
+
+<canvas bind:this={canvas} />
+
+<style>
+  canvas {
+    border: 1px solid #000;
+  }
+</style>
